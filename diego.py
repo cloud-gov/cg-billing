b65a07abc37d7c725052ed02532fc6b4ecb4e17a
Joshua Carp <jm.carp@gmail.com>
2017-05-03 18:41:33 -0400

    Aggregate billing events by guid; add notifications.

--- diego.py
+++ diego.py
@@ -5,6 +5,7 @@ import time
 import logging
 import datetime
 
+import requests
 import schedule
 import elasticsearch
 import elasticsearch.helpers
@@ -12,6 +13,16 @@ import elasticsearch.helpers
 logging.basicConfig(level=logging.INFO)
 
 metrics = ['memory_bytes', 'memory_bytes_quota']
+
+max_int = 2 ** 31 - 1
+
+aggs = {
+    metric: {'sum': {'field': 'containermetric.{}'.format(metric)}}
+    for metric in metrics
+}
+aggs.update({
+    'orgs': {'terms': {'field': '@cf.org'}}
+})
 
 query = {
     'size': 0,
@@ -24,11 +35,8 @@ query = {
     },
     'aggs': {
         'org': {
-            'terms': {'field': '@cf.org', 'size': 500},
-            'aggs': {
-                metric: {'avg': {'field': 'containermetric.{}'.format(metric)}}
-                for metric in metrics
-            },
+            'terms': {'field': '@cf.org_id', 'size': max_int},
+            'aggs': aggs,
         },
     },
 }
@@ -36,28 +44,46 @@ query = {
 
 def summarize(client, out_index, doc_type):
     date = datetime.date.today()
+    date = date.replace(month=date.month - 1)
     if date.day == 1:
-        in_index = 'logs-app-{}.*'.format(date.strftime('%Y.%m'))
-        res = client.search(index=in_index, body=query)
-        elasticsearch.helpers.bulk(
-            client,
-            get_bulk_docs(res, date),
-            index=out_index,
-            doc_type=doc_type,
-        )
+        try:
+            in_index = 'logs-app-{}.*'.format(date.strftime('%Y.%m'))
+            res = client.search(index=in_index, body=query)
+            elasticsearch.helpers.bulk(
+                client,
+                get_bulk_docs(res, date),
+                index=out_index,
+                doc_type=doc_type,
+            )
+        except Exception:
+            notify_slack('Failed to aggregate Diego metrics')
+            raise
+        notify_slack('Successfully aggregated Diego metrics')
+
+
+def notify_slack(text):
+    requests.post(
+        os.environ['SLACK_URL'],
+        json={
+            'username': os.environ['SLACK_USERNAME'],
+            'channel': os.environ['SLACK_CHANNEL'],
+            'icon_url': os.environ['SLACK_ICON_URL'],
+            'text': text,
+        },
+    ).raise_for_status()
 
 
 def get_bulk_docs(res, date):
     for org in res['aggregations']['org']['buckets']:
-        yield {
-            'update': {
-                '_id': '{}-{}'.format(date.strftime('%Y-%m'), org['key']),
-            },
+        doc = {
+            '_id': '{}-{}'.format(date.strftime('%Y-%m'), org['key']),
+            'org_id': org['key'],
+            'date': date,
+            'orgs': [each['key'] for each in org['orgs']['buckets']],
         }
-        doc = {'org': org['key'], 'date': date.strftime('%Y-%m-%d')}
         doc.update({metric: org[metric]['value'] for metric in metrics})
         logging.info(doc)
-        yield {'doc': doc}
+        yield doc
 
 
 if __name__ == '__main__':
@@ -65,7 +91,7 @@ if __name__ == '__main__':
     index = os.environ['BILL_INDEX']
     doc_type = os.environ['DOC_TYPE']
 
-    schedule.every().minute.do(summarize, client, index, doc_type)
+    schedule.every().day.do(summarize, client, index, doc_type)
 
     while True:
         schedule.run_pending()
